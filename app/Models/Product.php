727ef50af512fec4b7f3c54f04302a48fe11bf11
Vladislav Teteryatnikov <vladislav.teteryatnikov@gmail.com>
2025-01-21 17:35:46 +0300

    create model Product

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * Local scope to filter only available products.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAvailable($query)
+    {
+        return $query->where('status', '=', 'available');
+    }
+}
